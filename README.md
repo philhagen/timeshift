40bdf1e210c0446c1437ddc57f445db836e34806
Phil Hagen <phil@lewestech.com>
2015-10-14 22:55:19 -0400

    add "utcmode" option to convert HTTPD-like timestamps w/ offset to UTC; use regular expression to detect syslog timestamp instead of blindly using first 15 bytes of each line

--- README.md
+++ README.md
@@ -5,11 +5,12 @@ A python script to shift the timestamp on syslog data. Useful for forensicators 
 
 ```
 $ ./timeshift.py --help
-usage: timeshift.py [-h] -o OFFSET -i {second,minute,hour,day} [-y YEAR]
-                    [-r INFILE] [-w OUTFILE]
+usage: timeshift.py [-h] [-o OFFSET] [-i {second,minute,hour,day}] [-u]
+                    [-y YEAR] [-r INFILE] [-w OUTFILE]
 
 Shift the syslog date for all entries in an input data set by a specified
-interval of time. Offset and Interval options are required.
+interval of time. Offset and interval options are required unless using
+--utcmode.
 
 optional arguments:
   -h, --help            show this help message and exit
@@ -17,6 +18,8 @@ optional arguments:
                         Amount of time to shift (pos/neg integer)
   -i {second,minute,hour,day}, --interval {second,minute,hour,day}
                         Interval of time to shift
+  -u, --utcmode         Automatically parse timestamps such as
+                        "29/Aug/2013:23:46:33 -0400" to UTC
   -y YEAR, --year YEAR  Year to assume (default 2015)
   -r INFILE, --infile INFILE
                         Input file to process (default STDIN)
@@ -51,4 +54,26 @@ $ grep sendmail maillog | ./timeshift.py -o -23 -i second
 Jun  8 15:19:39 proxy sendmail[2295]: alias database /etc/aliases rebuilt by root
 Jun  8 15:19:39 proxy sendmail[2295]: /etc/aliases: 76 aliases, longest 10 bytes, 765 bytes total
 Jun  8 15:19:39 proxy sendmail[2300]: starting daemon (8.13.8): SMTP+queueing@01:00:00
+```
+
+Original contents of HTTPD access log file:
+```
+$ cat lewestech.com-access
+82.220.38.35 - - [11/Oct/2015:10:42:02 +0400] "POST /wp-login.php HTTP/1.1" 200 4697 "http://lewestech.com/wp-login.php" "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:23.0) Gecko/20100101 Firefox/23.0"
+208.115.113.85 - - [11/Oct/2015:11:27:15 +0400] "GET /clients/clients/waggies-by-maggie HTTP/1.1" 301 128 "-" "Mozilla/5.0 (compatible; DotBot/1.1; http://www.opensiteexplorer.org/dotbot, help@moz.com)"
+65.254.225.173 - - [11/Oct/2015:11:29:49 +0400] "POST /wp-login.php HTTP/1.1" 200 4697 "http://lewestech.com/wp-login.php" "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:23.0) Gecko/20100101 Firefox/23.0"
+82.239.166.225 - - [11/Oct/2015:11:58:49 +0400] "GET /tag/for572/ HTTP/1.1" 200 24951 "https://www.google.fr" "Mozilla/5.0 (Windows NT 6.3; WOW64; rv:41.0) Gecko/20100101 Firefox/41.0"
+82.239.166.225 - - [11/Oct/2015:11:58:50 +0400] "GET /wp-content/themes/lewestech/style.css HTTP/1.1" 200 10512 "http://lewestech.com/tag/for572/" "Mozilla/5.0 (Windows NT 6.3; WOW64; rv:41.0) Gecko/20100101 Firefox/41.0"
+82.239.166.225 - - [11/Oct/2015:11:58:51 +0400] "GET /wp-content/themes/lewestech/scripts/utils.js HTTP/1.1" 200 123 "http://lewestech.com/tag/for572/" "Mozilla/5.0 (Windows NT 6.3; WOW64; rv:41.0) Gecko/20100101 Firefox/41.0"
+```
+
+Convert HTTPD access log with UTC offset to UTC native
+```
+$ cat lewestech.com-access | ./timeshift.py -u
+82.220.38.35 - - [11/Oct/2015:06:42:02 +0000] "POST /wp-login.php HTTP/1.1" 200 4697 "http://lewestech.com/wp-login.php" "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:23.0) Gecko/20100101 Firefox/23.0"
+208.115.113.85 - - [11/Oct/2015:07:27:15 +0000] "GET /clients/clients/waggies-by-maggie HTTP/1.1" 301 128 "-" "Mozilla/5.0 (compatible; DotBot/1.1; http://www.opensiteexplorer.org/dotbot, help@moz.com)"
+65.254.225.173 - - [11/Oct/2015:07:29:49 +0000] "POST /wp-login.php HTTP/1.1" 200 4697 "http://lewestech.com/wp-login.php" "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:23.0) Gecko/20100101 Firefox/23.0"
+82.239.166.225 - - [11/Oct/2015:07:58:49 +0000] "GET /tag/for572/ HTTP/1.1" 200 24951 "https://www.google.fr" "Mozilla/5.0 (Windows NT 6.3; WOW64; rv:41.0) Gecko/20100101 Firefox/41.0"
+82.239.166.225 - - [11/Oct/2015:07:58:50 +0000] "GET /wp-content/themes/lewestech/style.css HTTP/1.1" 200 10512 "http://lewestech.com/tag/for572/" "Mozilla/5.0 (Windows NT 6.3; WOW64; rv:41.0) Gecko/20100101 Firefox/41.0"
+82.239.166.225 - - [11/Oct/2015:07:58:51 +0000] "GET /wp-content/themes/lewestech/scripts/utils.js HTTP/1.1" 200 123 "http://lewestech.com/tag/for572/" "Mozilla/5.0 (Windows NT 6.3; WOW64; rv:41.0) Gecko/20100101 Firefox/41.0"
 ```
--- timeshift.py
+++ timeshift.py
@@ -2,17 +2,26 @@
 import sys
 from datetime import datetime, timedelta, date
 import argparse
+import re
 
 intervals = ('second', 'minute', 'hour', 'day')
 curryear = date.today().year
 
-parser = argparse.ArgumentParser(description='Shift the syslog date for all entries in an input data set by a specified interval of time.  Offset and Interval options are required.')
-parser.add_argument('-o', '--offset', help='Amount of time to shift (pos/neg integer)', required=True, type=int)
-parser.add_argument('-i', '--interval', help='Interval of time to shift', required=True, choices = intervals)
+tzts_re = re.compile('(?P<datestring>(?P<date>[0-9]{2}/[A-Za-z]{3}/[0-9]{4}):(?P<time>[0-9]{2}:[0-9]{2}:[0-9]{2}) (?P<offset_dir>[+-])(?P<offset>[0-9]{4}))')
+syslog_re = re.compile('(?P<datestring>(?P<date>[A-Za-z]{3} [0-9 ]{2}) (?P<time>[0-9]{2}:[0-9]{2}:[0-9]{2}))')
+
+parser = argparse.ArgumentParser(description='Shift the syslog date for all entries in an input data set by a specified interval of time.  Offset and interval options are required unless using --utcmode.')
+parser.add_argument('-o', '--offset', help='Amount of time to shift (pos/neg integer)', type=int)
+parser.add_argument('-i', '--interval', help='Interval of time to shift', choices = intervals)
+parser.add_argument('-u', '--utcmode', help='Automatically parse timestamps such as "29/Aug/2013:23:46:33 -0400" to UTC', action='store_true', default=False)
 parser.add_argument('-y', '--year', help='Year to assume (default %d)' % curryear, default=curryear, type=int)
 parser.add_argument('-r', '--infile', help='Input file to process (default STDIN)')
 parser.add_argument('-w', '--outfile', help='Output file to create - will be overwritten if exists (default STDOUT)')
 args = parser.parse_args()
+
+if args.utcmode == None and (args.offset == None or args.interval == None):
+    stderr.write('ERROR: Offset and interval options are both required when not using --utcmode\n')
+    sys.exit(2)
 
 # open the input file or use STDIN if not specified
 if args.infile:
@@ -34,29 +43,56 @@ if args.outfile:
 else:
     outfile = sys.stdout
 
-# establish a timedelta object that defines the requested offset and interval
-if args.interval == 'second':
-    offset = timedelta(seconds = args.offset)
-elif args.interval == 'minute':
-    offest = timedelta(minutes = args.offset)
-elif args.interval == 'hour':
-    offset = timedelta(hours = args.offset)
-elif args.interval == 'day':
-    offset = timedelta(hours = args.offset * 24)
-
 # iterate through each line of the input data
 for line in infile:
-    # syslog timestamp is the first 15 characters by default (e.g. "Nov 20 15:20:02")
-    # then add a year, which may matter in a leap year
-    origtimestring = line[0:15] + ' %d' % args.year
-    origtime = datetime.strptime(origtimestring, '%b %d %X %Y')
+    if args.utcmode:
+        parts = tzts_re.search(line)
 
-    # rest of the line will remain the same
-    remainder = line[15:]
+        origtimestring = '%s %s' % (parts.group('date'), parts.group('time'))
+        origtime = datetime.strptime(origtimestring, '%d/%b/%Y %X')
 
-    # calculate the new time in a time object
-    newtime = origtime + offset
+        offset_dir = parts.group('offset_dir')
+        offset = parts.group('offset')
+        offset_hr = int(offset[0:2])
+        offset_min = int(offset[2:4])
+        offset = timedelta(hours=offset_hr, minutes = offset_min)
 
-    # write the new line, including a formatted timestamp for the new time
-    # the %-2d format string is a two-character wide format that omits any leading zeroes (uses space instead) - nice!
-    outfile.write('%s%s' % (newtime.strftime('%b %-2d %X'), remainder))
+        # we are correcting the time zone to UTC, so the math is opposite the TZ direction
+        if offset_dir == '+':
+            newtime = origtime - offset
+        else:
+            newtime = origtime + offset
+
+        newtimestring = newtime.strftime('%d/%b/%Y:%X')
+        newline = tzts_re.sub(newtimestring+' +0000\0', line)
+
+    else:
+        # establish a timedelta object that defines the requested offset and interval
+        if args.interval == 'second':
+            offset = timedelta(seconds = args.offset)
+        elif args.interval == 'minute':
+            offest = timedelta(minutes = args.offset)
+        elif args.interval == 'hour':
+            offset = timedelta(hours = args.offset)
+        elif args.interval == 'day':
+            offset = timedelta(hours = args.offset * 24)
+
+        # syslog timestamp is the first 15 characters by default (e.g. "Nov 20 15:20:02")
+        # then add a year, which may matter in a leap year
+        parts = syslog_re.search(line)
+
+        origtimestring = '%s %s' % (parts.group('datestring'), args.year)
+        origtime = datetime.strptime(origtimestring, '%b %d %X %Y')
+
+        # calculate the new time in a time object
+        newtime = origtime + offset
+
+        # reconstruct the new line
+        # the %-2d format string is a two-character wide format that omits any leading zeroes (uses space instead) - nice!
+        newtimestring = newtime.strftime('%b %-2d %X')
+        newline = syslog_re.sub(newtimestring+'\0', line)
+
+        #'%s%s' % (newtime.strftime('%b %-2d %X'), remainder)
+
+    # write the new line, including a formatted timestamp for the corrected time
+    outfile.write(newline)
